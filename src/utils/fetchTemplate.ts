2eaf48beed60298242544104f5dd2df355296015
nhutphi456 <vonhutphi>
2023-09-21 10:06:34 +0700

    optimize load template

--- src/utils/fetchTemplate.ts
+++ src/utils/fetchTemplate.ts
@@ -1,8 +1,15 @@
 const PATH = "./components";
 
+const templates = JSON.parse(localStorage.getItem("templates") ?? "{}");
+
 export async function loadTemplate(url: string): Promise<string> {
+  if (templates && url in templates) return templates[url];
+
   const response = await fetch(`${PATH}${url}`);
   const html = await response.text();
-  
+
+  templates[url] = html;
+  localStorage.setItem("templates", JSON.stringify(templates));
+
   return html;
 }
